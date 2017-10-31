901bf2feeffd1b94da311d4324dc4204cd81f599
Apiwat Suksuwan <apiwat.s@zanroo.com>
2017-10-31 14:27:43 +0700

    updated to express

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-var http = require('http');
+'use strict';
 
-// configure our HTTP server
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello Jenkins\n");
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello Jenkins\n');
 });
 
-// listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
